b91d00be239b11edc70c05abcb9ecad3788f7e8c
dcanitez <dilaycanitez@gmail.com>
2021-12-15 18:52:13 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace WFA_WebBrowser_NetFramework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.cmbUrlList = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.wbBrowser = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtURL);
+            this.panel1.Controls.Add(this.btnGo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(626, 31);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnGo.Location = new System.Drawing.Point(557, 0);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(69, 31);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go!";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtURL
+            // 
+            this.txtURL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtURL.Location = new System.Drawing.Point(0, 0);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(557, 30);
+            this.txtURL.TabIndex = 1;
+            this.txtURL.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtURL_KeyDown);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.cmbUrlList);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 31);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(626, 26);
+            this.panel3.TabIndex = 2;
+            // 
+            // cmbUrlList
+            // 
+            this.cmbUrlList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cmbUrlList.FormattingEnabled = true;
+            this.cmbUrlList.Location = new System.Drawing.Point(0, 0);
+            this.cmbUrlList.Name = "cmbUrlList";
+            this.cmbUrlList.Size = new System.Drawing.Size(626, 24);
+            this.cmbUrlList.TabIndex = 0;
+            this.cmbUrlList.Text = "History..";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.wbBrowser);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 57);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(626, 386);
+            this.panel2.TabIndex = 3;
+            // 
+            // wbBrowser
+            // 
+            this.wbBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wbBrowser.Location = new System.Drawing.Point(0, 0);
+            this.wbBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbBrowser.Name = "wbBrowser";
+            this.wbBrowser.Size = new System.Drawing.Size(626, 386);
+            this.wbBrowser.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 443);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "MyBrowser";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ComboBox cmbUrlList;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.WebBrowser wbBrowser;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_WebBrowser_NetFramework
+{
+    public partial class Form1 : Form
+    {
+        List<string> urlList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            NavigateToBrowser();
+        }
+
+        private void NavigateToBrowser()
+        {
+            string url = txtURL.Text.Trim();
+
+            if (string.IsNullOrEmpty(url))
+            {
+                MessageBox.Show("Please Write a proper URL to Go!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (url.StartsWith("https://") == false)
+            {
+                url = "https://" + url;
+                txtURL.Text = url;
+            }
+
+            wbBrowser.Navigate(url);
+
+            if (!urlList.Contains(url))
+            {
+                urlList.Add(url);
+            }
+
+            FillComboBox();
+            
+        }
+
+        private void FillComboBox()
+        {
+            cmbUrlList.DataSource = null;
+            cmbUrlList.DataSource = urlList;
+            cmbUrlList.SelectedIndex = -1;
+            cmbUrlList.Text = "Search History..";
+        }
+
+        private void txtURL_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Enter)
+            {
+                NavigateToBrowser();
+            }
+        }
+    }
+}
